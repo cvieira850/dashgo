c2e67d9d27e4ab2063a2c0b9ef084b71d9084712
Caio Vieira <contato@caiovieira.com.br>
2022-04-28 10:48:09 -0300

    feat: add responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData =true} : ProfileProps) {
   return (
     <Flex alignItems="center">
+      { showProfileData && (
         <Box marginRight="4" textAlign="right">
           <Text>Caio Vieira</Text>
           <Text color="gray.300" fontSize="small">contato@caiovieira.com.br</Text>
         </Box>
+      )}
         <Avatar size="md" name="Caio Vieira" src="https://github.com/cvieira850.png"/>
     </Flex>
   );
